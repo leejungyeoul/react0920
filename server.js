add2e3552cea404620e9bd3a227e10bf08547cfa
ljung5 <01032883398a@gmail.com>
2020-09-27 17:22:43 +0900

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var swtoolRouter = require("./routes/SwtoolRout");
 var fileuploadRouter = require("./routes/UploadRout");
+var usersRouter = require("./routes/UsersRout");
+var MailRout = require("./routes/MailRout");
 require("./routes/BatchRout");
 
 var app = express();
@@ -13,6 +14,9 @@ app.use('/users', usersRouter);
 app.use("/api/Swtool", swtoolRouter);
 app.use("/api/upload", fileuploadRouter);
 app.use(express.static("./uploads"));
+app.use("/api/register", usersRouter);
+app.use("/api/LoginForm", usersRouter);
+app.use("/api/mail", MailRout);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
